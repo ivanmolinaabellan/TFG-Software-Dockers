3cdee95e310fc313193e56296b4c3978e18d21bd
Iván <ivan.molina.aebllan@alumnos.upm.es>
2023-10-11 05:30:33 +0200

    all added

---
+++ router/entrypoints/ep_routerA.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+service syslog-ng start
+service frr start
+
+vtysh << EOF
+conf t
+log file /shared-volume/frr/frr1.log
+router ospf
+network 10.100.0.0/24 area 0
+network 10.200.0.0/30 area 0
+network 10.1.0.0/30 area 0
+network 10.100.10.0/24 area 0
+end
+EOF
+
+
+
+/bin/sleep infinity
